f6bbbb126907ed88eec035d3b24c8ff9aa2f649e
marcwayn8 <juniormarch48@gmail.com>
2023-01-16 19:43:37 -0500

    lab all done

--- exercises.js
+++ exercises.js
@@ -1,37 +1,52 @@
-// Question 1
-const isUpperCase = () => {
-
+const isUpperCase = (string) => {
+  return string.split('').every((char) => char === char.toUpperCase() && /[a-zA-Z]/.test(char));
 };
 
-// Question 2
-const removeVowels = () => {
-
+const removeVowels = (array) => {
+  return array.map((string) => string.replace(/[aeiouAEIOU]/g, ''));
 };
 
-// Question 3
-const wordCap = () => {
-
+const wordCap = (string) => {
+  return string.split(' ').map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 };
 
-// Question 4
-const swapCase = () => {
-
+const swapCase = (string) => {
+  return string.split('').map((char) => char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()).join('');
 };
 
-// Question 5
-const staggeredCase = () => {
-
+const staggeredCase = (string) => {
+  let isUpper = true;
+  return string.split('').map((char) => {
+      if(/[a-zA-Z]/.test(char)) {
+          isUpper = !isUpper;
+          return isUpper ? char.toUpperCase() : char.toLowerCase();
+      } else {
+          return char;
+      }
+  }).join('');
 };
 
-// Question 6
-const wordLengths = () => {
-
+const wordLengths = (string) => {
+  return string.split(' ').map((word) => word + ' ' + word.length);
 };
 
-// Question 7
-const searchWord = () => {
 
-};
+
+const searchWord = (word, text) => {
+  let count = 0;
+  let lowerText = text.toLowerCase();
+  let lowerWord = word.toLowerCase();
+  let startIndex = 0;
+  while (startIndex !== -1) {
+    startIndex = lowerText.indexOf(lowerWord, startIndex);
+    if (startIndex !== -1) {
+      count++;
+      startIndex += word.length;
+    }
+  }
+  return count;
+}
+
 
 
 // The code below ensures that this file can talk to our test file.
--- exercises.test.js
+++ exercises.test.js
@@ -1,10 +1,50 @@
 const exercises = require('./exercises');
 
-//write tests in this file
-
-/* Example Test 
-test('Question 0: add', () => {
-  expect(exercises.add(1, 2)).toEqual(3);
-  expect(exercises.add(1, -1)).toEqual(0);
+test('isUpperCase', () => {
+  expect(isUpperCase('t')).toBe(false);
+  expect(isUpperCase('T')).toBe(true);
+  expect(isUpperCase('Four Score')).toBe(false);
+  expect(isUpperCase('FOUR SCORE')).toBe(true);
+  expect(isUpperCase('4SCORE!')).toBe(true);
+  expect(isUpperCase('')).toBe(true);
 });
-*/
+
+test('removeVowels', () => {
+  expect(removeVowels(['abcdefghijklmnopqrstuvwxyz'])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
+  expect(removeVowels(['green', 'YELLOW', 'black', 'white'])).toEqual(["grn", "YLLW", "blck", "wht"]);
+  expect(removeVowels(['ABC', 'AEIOU', 'XYZ'])).toEqual(["BC", "", "XYZ"]);
+});
+
+test('wordCap', () => {
+  expect(wordCap('four score and seven')).toBe("Four Score And Seven");
+  expect(wordCap('the javaScript language')).toBe("The Javascript Language");
+  expect(wordCap('this is a "quoted" word')).toBe('This Is A "quoted" Word');
+});
+
+test('swapCase', () => {
+  expect(swapCase('CamelCase')).toBe("cAMELcASE");
+  expect(swapCase('Tonight on XYZ-TV')).toBe("tONIGHT ON xyz-tv");
+});
+
+test('staggeredCase', () => {
+  expect(staggeredCase('I Love Launch School!')).toBe("I lOvE lAuNcH sChOoL!");
+  expect(staggeredCase('ALL CAPS')).toBe("AlL cApS");
+  expect(staggeredCase('ignore 77 the 444 numbers')).toBe("IgNoRe 77 ThE 444 nUmBeRs");
+});
+
+test('wordLengths', () => {
+  expect(wordLengths('cow sheep chicken')).toEqual(["cow 3", "sheep 5", "chicken 7"]);
+  expect(wordLengths('baseball hot dogs and apple pie')).toEqual(["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]);
+  expect(wordLengths("It ain't easy, is it?")).toEqual(["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]);
+  expect(wordLengths('Supercalifragilisticexpialidocious')).toEqual(["Supercalifragilisticexpialidocious 34"]);
+  expect(wordLengths('')).toEqual([]);
+});
+
+test('searchWord', () => {
+  let text = 'D Smoke is humble. The Inglewood native exudes an aura of maturation, needed for his quick ascension into popular culture as the first winner of Rhythm + Flow, Netflix’s hip-hop reality competition centered on the discovery of hip-hop’s next star. His signature authenticity shone throughout the 10-episode series and international audiences were drawn to his charisma as he proudly rapped about his lived experiences as a young black man in Inglewood.';
+  expect(searchWord('his', text)).toBe(4);
+  expect(searchWord('hip-hop', text)).toBe(1);
+  expect(searchWord('Flow', text)).toBe(1);
+  expect(searchWord('Inglewood', text)).toBe(2);
+  expect(searchWord('the', text)).toBe(2);
+});
